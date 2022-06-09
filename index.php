815d021880570a082462af35969dd6fd93576391
Annylys Medeiros <75506615+AnnylysMedeiros@users.noreply.github.com>
2022-06-09 18:37:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include_once('src/main/resources/templates/index.html'); ?>
 <?php include_path('src/main/resources/templates/'); ?>
+<?php include_path('src/main/resources/static/js/'); ?>
 
 
